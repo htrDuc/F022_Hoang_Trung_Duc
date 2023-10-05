75825e61a757af36eea4611f0627d46748c30d5a
Duc Hoang Trung <ducht@smartosc.com>
2023-10-05 12:46:33 +0700

    problem1

--- problem1/index.js
+++ problem1/index.js
@@ -1,4 +1,4 @@
-var sum_to_n_a = function (n) {
+const sum_to_n_a = function (n) {
   let sum = 0;
   for (let i = 1; i <= n; i++) {
     sum += i;
@@ -6,11 +6,17 @@ var sum_to_n_a = function (n) {
   return sum;
 };
 
-var sum_to_n_b = function (n) {
+const sum_to_n_b = function (n) {
+  const arr = Array.from({ length: n }, (_, i) => i + 1);
+  return arr.reduce((prev, cur) => prev + cur, 0);
+};
+
+const sum_to_n_c = function (n) {
   return (n * (n + 1)) / 2;
 };
 
-var sum_to_n_c = function (n) {
-  const arr = Array.from({ length: n }, (v, i) => i + 1);
-  return arr.reduce((prev, cur) => prev + cur, 0);
+const sum_to_n_d = function (n) {
+  if (n === 0) return 0;
+
+  return sum_to_n_d(n - 1) + n;
 };
